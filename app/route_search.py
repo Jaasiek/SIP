df5d5401de5c36e29c75268333480b1c629b93c9
Jasiek <u119_jangaw_waw@technischools.com>
2025-03-17 12:37:45 +0100

    searching improved (returning stops name for line & type)

--- app/route_search.py
+++ app/route_search.py
@@ -3,7 +3,7 @@ import json
 final_path = "data/routes/stops.json"
 
 
-def getting_route(line: str, path: str, variant: str = None) -> str:
+def getting_route(line: str, path: str, variant: str = None):
     with open(path, "r", encoding="utf-8") as file:
         data = json.load(file)
 
@@ -11,15 +11,21 @@ def getting_route(line: str, path: str, variant: str = None) -> str:
     if line_data is None:
         return f"No data found for line {line}"
 
-    if variant:
+    if variant != "0":
         result = line_data.get(variant)
         if result is None:
             return f"No data found for variant {variant} of line {line}"
 
-        return json.dumps(result, indent=4, ensure_ascii=False)
+        # Extract "nazwa_zespolu" values
+        stops = [stop_data["nazwa_zespolu"] for stop_data in result.values()]
+        return json.dumps(stops, indent=4, ensure_ascii=False)
 
     else:
         return json.dumps(line_data, indent=4, ensure_ascii=False)
 
 
-print(getting_route("33", final_path))
+while True:
+    line = input("Line: ")
+    type = input("Type: ")
+
+    print(getting_route(line, final_path, type))
