2dce4dec968d9f89d6a24133e388348df8f5b9dc
Jasiek <u119_jangaw_waw@technischools.com>
2025-03-26 22:44:28 +0100

    network visible

--- app/app.py
+++ app/app.py
@@ -202,4 +202,6 @@ def metro_screen():
 
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(
+        app, host="0.0.0.0", port=5000, debug=False, allow_unsafe_werkzeug=True
+    )
